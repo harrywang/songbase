23839b6d33b68e4fdf37dd99df20e34802156b30
Harry Wang <harryjwang@gmail.com>
2017-11-06 19:59:59 -0500

    added static and templates

--- songbase.py
+++ songbase.py
@@ -1,25 +1,28 @@
-from flask import Flask
+from flask import Flask, render_template
 app = Flask(__name__)
 
 
 @app.route('/')
 def index():
     # return HTML
-    return "<h1>this is the index page!<h1>"
+    # return "<h1>this is the index page!<h1>"
+    return render_template('index.html')
 
 
 @app.route('/users')
 def get_all_users():
-    return "<h2>this is the page for all users</h2>"
+    # return "<h2>this is the page for all users</h2>"
+    return render_template('user-all.html')
 
 
 @app.route('/users/<string:name>/')
 def get_user_name(name):
     # return "hello " + name
-    return "Hello %s, this is %s" % (name, 'administrator')
+    # return "Hello %s, this is %s" % (name, 'administrator')
+    return render_template('user.html', name=name)
 
 
-@app.route('/songs/<int:id>/')
+@app.route('/song/<int:id>/')
 def get_song_id(id):
     # return "This song's ID is " + str(id)
     return "Hi, this is %s and the song's id is %d" % ('administrator', id)
